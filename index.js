7453996ebb53a4fb2859f32aabd61dc07943007b
Kundan <kr924915@gmail.com>
2026-02-16 00:37:37 +0530

    feat: add POST /url endpoint to generate short url

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app=express();
 const PORT =8001;
+const urlRoute = require('./routes/url');
+const connectToMongoDB = require('./connect');
 
+app.use(express.json());
+app.use("/url",urlRoute);
+connectToMongoDB('mongodb://localhost:27017/url-shortener').then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+    console.error("Failed to connect to MongoDB", err);
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
