879b6f8c9472c9a5d18abf9b3ae38d58afeba637
Kundan <kr924915@gmail.com>
2026-02-17 03:11:17 +0530

    chore: remove legacy JS files after TS migration

--- routes/url.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { handleGenerateShortUrl } = require("../url");
-const router = express.Router();
-router.post("/", handleGenerateShortUrl);
-
-module.exports = router;
