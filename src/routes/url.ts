782aa79994a77bef2f879b2fdcebbc8ae4aa27b1
Kundan <kr924915@gmail.com>
2026-02-21 16:31:09 +0530

    Revert "Merge pull request #3 from KundanPRATAPSING/fix/linting-updates"
    This reverts commit b4077d5b1684defc9f9066bf4a227865318d62d5, reversing
    changes made to 27dc5b36d9bcae8fa8c8c4e8757e6dc73e12954a.
    
    git push origin main#	modified:   pnpm-lock.yaml
    git push origin main#

--- src/routes/url.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { handleGenerateShortUrl } from "../controllers/url";
-
-const router = express.Router();
-router.post("/", handleGenerateShortUrl);
-
-export default router;
